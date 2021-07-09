7970aa7c6ad127589d9f08aaa3715584ba51655f
danbiilee <danbi.db@gmail.com>
2021-07-09 10:59:38 +0900

    [FEAT] ThemeProvider 적용

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { HashRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
 import App from "../src/components/App";
+import { theme } from "./utils/theme";
 
 ReactDOM.render(
   <HashRouter>
-    <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
   </HashRouter>,
   document.getElementById("root")
 );
---
+++ src/utils/theme/index.js
@@ -0,0 +1,8 @@
+const size = {
+  baseSpace: "1.3rem",
+  componentSpace: "1rem",
+};
+
+export const theme = {
+  size,
+};
