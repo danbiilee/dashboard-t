e4f191f8c90c6b7dd89d100e1ab98caa25e2a468
danbiilee <danbi.db@gmail.com>
2021-07-13 07:19:05 +0900

    [FIX] min-height 삭제

--- src/components/App/AppClient.js
+++ src/components/App/AppClient.js
@@ -13,9 +13,7 @@ const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   overflow-y: auto;
-  /* min-width: 1790px; */
   min-width: 1320px;
-  min-height: 970px;
   height: 100%;
   background-color: #f8f9fa;
 `;
