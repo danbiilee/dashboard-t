2bc5f967b0713c3dd99863f8924661a666f9df0e
danbiilee <danbi.db@gmail.com>
2021-08-17 14:34:36 +0900

    [FIX] 환경변수 적용 수정(#2)

--- src/service/firebase.js
+++ src/service/firebase.js
@@ -1,11 +1,10 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
-import * as process from "../../env";
 
 const firebaseConfig = {
-  apiKey: process.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.REACT_APP_FIREBASE_PROJECT_ID,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
